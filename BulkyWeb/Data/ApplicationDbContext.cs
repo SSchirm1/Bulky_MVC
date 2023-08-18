9adc9afc92eab29fdd5daf592516bfd66f796426
Sondre Schirmer-Mikalsen <sondremikalsen1@gmail.com>
2023-08-18 13:56:33 +0200

    first migration to local database

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
@@ -14,7 +15,7 @@ namespace BulkyWeb.Data
 
         }
 
-
+        public DbSet<Category> Categories { get; set; }
 
 
 
